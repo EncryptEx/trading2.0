4f572c2fbb46b5d07697ae99a18a88bba0634bbf
EncryptEx <limusina10@gmail.com>
2023-01-01 17:36:06 +0100

    hotfix credentials file

--- src/credentials.example.php
+++ src/credentials.example.php
@@ -8,12 +8,13 @@ $charset = 'UTF8MB4';
 // Secure random string key that will help in encrypting content 
 $privKey = '';
 
-$desiredAuthKey = 'test';
-
 // google ReCaptcha API key
 $captchaSecret = '';
 
 // Password that you will need to set to do various automated actions
 $desiredAuthKey = '';
+
+// base price for non-registered countries.
+$countryBasePrice = 10000;
 
 $FEE = 0.05;
