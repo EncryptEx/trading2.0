2cd067d3ac4e7450d46a9da3c8221edbdfed1499
EncryptEx <limusina10@gmail.com>
2023-03-28 15:17:28 +0200

    added specific endpoint

--- src/api/index.php
+++ src/api/index.php
@@ -5,17 +5,32 @@ $uri = explode( '/', $uri );
 
 
 header('Content-type: application/json; charset=utf-8');
-// example.com/api/market/list
-if ((isset($uri[2]) && $uri[2] == 'market') && isset($uri[3]) && $uri[3] == 'list') {
+// /api/market/all
+if ((isset($uri[2]) && $uri[2] == 'market') && isset($uri[3]) && $uri[3] == 'all') {
     
     $markets = [];
     $preMarkets = getMarkets()->fetchAll();
     foreach ($preMarkets as $market) {
-
-        array_push($markets, ['name'=>$market['name'], "icon" => $market['logo']]);
+        
+        array_push($markets, ['id'=>$market['id'], 'name'=>$market['name'], "icon" => $market['logo']]);
     }
     echo json_encode($markets);
     exit();
+}
+
+// /api/market/{id}
+if ((isset($uri[2]) && $uri[2] == 'market') && isset($uri[3]) && $uri[3] != 'all') {
+    $requestedID = $uri[3];
+    $preMarkets = getMarkets()->fetchAll();
+    foreach ($preMarkets as $market) {
+        if($market['id'] == $requestedID){
+            echo json_encode(['id'=>$market['id'], 'name'=>$market['name'], "icon" => $market['logo']]);
+            exit();
+        }
+    }
+    echo json_encode(['error'=>TRUE, 'message'=>'Market not found']);
+    exit();
+
 }
 header("HTTP/1.1 404 Not Found");
 echo json_encode(['error'=>TRUE, 'message'=>'Endpoint not found']);
